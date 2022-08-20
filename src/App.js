dc03c68970f5268c2080512bf64fcc00360c5efa
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-08-19 23:45:01 -0400

    Set routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Homepage from "./components/Homepage";
+
+import "./styles/App.scss";
+
 function App() {
   return (
-    <div className="App">
-      khaled
+    <div className="main">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
