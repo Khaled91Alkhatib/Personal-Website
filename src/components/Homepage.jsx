5f282b66603962977e295bd60123538c98ea49b6
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-08-20 23:22:39 -0400

    Create refs to scroll on button click and pass them to components

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 
-import Navbar from "./Navbar"
-import Introduction from './Introduction'
+import Navbar from "./Navbar";
+import Introduction from './Introduction';
+import AboutMe from './AboutMe';
+import Work from './Work';
 
+import "../styles/Homepage.scss"
 const Homepage = () => {
+  const myRef = useRef(null)
+
+  const executeScroll = () => myRef.current.scrollIntoView()
+  
+  const scrollToDiv = (ref) => window.scrollTo(0, ref.current.offsetTop);
+  // These are for the 'About' button
+  const el1 = useRef();
+  const el2 = useRef();
+
+  // These are for the 'Work' button
+  // const el3 = useRef()
+  // const el4 = useRef()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <div>
-      <Navbar />
-      <Introduction />
+      <div className='first-view'>
+        <Navbar reference={[el1]} onClick={executeScroll} click={() => scrollToDiv(el2)} />
+        <Introduction />
+      </div>
+      <AboutMe reference={el2} />
+      <Work refProp={myRef} />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
