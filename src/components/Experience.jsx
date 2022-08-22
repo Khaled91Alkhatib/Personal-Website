9f9a00451b351cd4feaf695025118979addebabe
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-08-22 13:54:37 -0400

    Add a list for experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,9 +7,18 @@ const Experience = (props) => {
     <div ref={props.myExperienceRef} className='main-experience'>
       <h2 className='experience-title exp-count'>Where I've Worked</h2>
       <div className='work-data'>
-        While I was working on my Bachelor's Degree in dentisry, I worked for almost one year as a dental assistant.<br />
-        After graduation, I was not able to work in my profession due to circumstances out of my hand.<br /> However, I was fortunate enough to find a job as a sales representative which I did for two years in two different companies until I moved to Canada in 2020. The companies that I worked in are Sawary Trading and Contracting Company and Damas Medical Company which are both located in Jeddah, Kingdom of Saudi Arabia.<br />
-        Within these two years, I also did some minor short-term tasks mostly involving data entry.
+        During my dentistry bachelor's degree studies, I worked for almost one year as a dental assistant.<br /><br />
+        After graduation, I was not able to work in my profession due to circumstances out of my hand.<br /> However, I was fortunate enough to find a job as a sales representative which I did for two years in two different companies until I moved to Canada in 2020. The companies that I worked in were Sawary Trading and Contracting Company and Damas Medical Company which are both located in Jeddah, Kingdom of Saudi Arabia.<br />
+        Within these two years, I also did some minor short-term tasks mostly involving data entry.<br /><br />
+        While I was holding the position of a sales representative, my job included:
+        <ul>
+          <li>Meeting sales goals.</li>
+          <li>Preparing and submitting sales contracts for orders.</li>
+          <li>Contributes to team effort by accomplishing related results as needed.</li>
+          <li>Answering client questions about credit terms, products, prices, and availability.</li>
+          <li>Visiting clients and potential clients to evaluate needs or promote products and services.</li>
+          <li>Resolving customer complaints by discussing problems, working on solutions and making recommendations to management.</li>
+        </ul>
       </div>
     </div>
   )
